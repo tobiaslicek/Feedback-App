b1ba156313b75f353c2ee9cd2499591394fc1e0d
Tobiáš Licek <tobiaslicek@seznam.cz>
2023-10-12 10:59:04 +0200

    add feedback array

--- feedback.php
+++ feedback.php
@@ -1,4 +1,27 @@
 <?php include 'inc/header.php'; ?>
+
+<?php
+$feedback = [
+  [
+    'id' => '1',
+    'name' => 'Beth Williams',
+    'email' => 'beth@gmail.com',
+    'body' => 'Thank you!',
+    [],
+    'id' => '2',
+    'name' => 'Walt Williams',
+    'email' => 'walt@will.com',
+    'body' => 'Hello, I really enjoyed your course.',
+  ],
+  [
+    'id' => '3',
+    'name' => 'Bill',
+    'email' => 'bill@remail.com',
+    'body' => 'Worth it',
+  ],
+];
+?>
+
 <h2>Feedback</h2>
 
 <div class="card my-3">

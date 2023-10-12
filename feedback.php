1914df9bfd496257ab1e0536643e32c93cbc540a
Tobiáš Licek <tobiaslicek@seznam.cz>
2023-10-12 11:10:43 +0200

    Style Feedback Cards

--- feedback.php
+++ feedback.php
@@ -1,44 +1,41 @@
 <?php include 'inc/header.php'; ?>
 
 <?php
-$feedback = [
-  [
-    'id' => '1',
-    'name' => 'Beth Williams',
-    'email' => 'beth@gmail.com',
-    'body' => 'Thank you!',
-    [],
-    'id' => '2',
-    'name' => 'Walt Williams',
-    'email' => 'walt@will.com',
-    'body' => 'Hello, I really enjoyed your course.',
-  ],
-  [
-    'id' => '3',
-    'name' => 'Bill',
-    'email' => 'bill@remail.com',
-    'body' => 'Worth it',
-  ],
-];
+// $feedback = [
+//   [
+//     'id' => '1',
+//     'name' => 'Beth Williams',
+//     'email' => 'beth@gmail.com',
+//     'body' => 'Thank you!',
+//   ],
+//   [
+//     'id' => '2',
+//     'name' => 'Walt Williams',
+//     'email' => 'walt@will.com',
+//     'body' => 'Hello, I really enjoyed your course.',
+//   ],
+//   [
+//     'id' => '3',
+//     'name' => 'Bill',
+//     'email' => 'bill@remail.com',
+//     'body' => 'Worth it',
+//   ]
+// ];
+
 ?>
 
 <h2>Feedback</h2>
 
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-  </div>
-</div>
+<?php if (empty($feedback)) : ?>
+  <p class="lead mt3">There is no feedback</p>
+<?php endif; ?>
 
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-  </div>
-</div>
-
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-  </div>
-</div>
-<?php include 'inc/footer.php'; ?>
+<?php foreach ($feedback as $item) : ?>
+  <div class="card my-3 w-75">
+    <div class="card-body text-center">
+      <?php echo $item['body']; ?>
+      <div class="text-secondary mt-2">
+        By <?php echo $item['name']; ?>
+      </div>
+    </div>
+  </div <?php endforeach; ?> <?php include 'inc/footer.php'; ?>
